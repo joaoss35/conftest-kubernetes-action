a06e05953d9d3ce0ea11c6482c581e1e4463e53c
João Pacheco <jmdcp02@gmail.com>
2021-06-07 00:48:56 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
-#!/bin/sh
+#!/bin/bash -Ee
+
 
 echo "
   _____             __ _            _     _        _____ _             _   _
@@ -10,35 +11,42 @@ echo "
                                                                                     __/ |
                                                                                    |___/
 "
-set -e
 
 # ------ Conftest Command Arguments ------
-$COMBINE
-$COMMENT
-$NAMESPACE
-$OUTPUT
-$PARSER
-$FILE
-$POLICY
-$TOKEN
-$TRACE
-$UPDATE
+#$COMBINE
+#$COMMENT
+#$NAMESPACE
+#$OUTPUT
+#$PARSER
+#$FILE
+#$POLICY
+#$TOKEN
+#$TRACE
+#$UPDATE
 
 # ------ Variables ------
-$CONFTEST_COMMAND
-$CONFTEST_OUTPUT
-$SUCCESS
+#$CONFTEST_COMMAND
+#$CONFTEST_OUTPUT
+#$SUCCESS
 
-set +e
 # ------ Main ------
 cd "${GITHUB_WORKSPACE}/${WORKING_DIR}"
 
-# Run Conftest test command
-CONFTEST_COMMAND="/usr/local/bin/conftest test --combine=$COMBINE -n ${NAMESPACE} --parser=${PARSER} -p ${POLICY} -o ${OUTPUT} -u ${UPDATE} --trace=${TRACE} ${FILE}"
-OUTPUT=$(sh -c "${CONFTEST_COMMAND}" 2>&1)
-SUCCESS=$?
+OPTIONS=("$COMBINE" "$NAMESPACE" "$OUTPUT" "$PARSER" "$FILE" "$POLICY" "$TRACE" "$UPDATE")
 
-set -e
+# Check if all variables are defined
+for i in "${OPTIONS[@]}";
+do
+    if [ -z "${i}" ]; then
+        echo "Not all Conftest options are defined. Exiting..."
+        exit 1
+    fi
+done
+
+# Run Conftest test command
+CONFTEST_COMMAND="/usr/local/bin/conftest test --combine=$COMBINE -n $NAMESPACE --parser=$PARSER -p $POLICY -o $OUTPUT -u $UPDATE --trace=$TRACE $FILE"
+CONFTEST_OUTPUT="$(sh -c "${CONFTEST_COMMAND}" 2>&1)"
+SUCCESS=$?
 
 # Check if Conftest run successfully
 if [ ${SUCCESS} -eq 0 ]; then
@@ -49,5 +57,3 @@ else
   echo "Conftest has failed validating your files"
   exit 1
 fi
-
-set +e
